1edcd288783dde68cc6976b0b6c2db9afd5a4008
shiyun-ho <serene.hoshiyun@gmail.com>
2023-04-05 16:48:10 +0800

    final commit

--- database/schema.sql
+++ database/schema.sql
@@ -6,14 +6,16 @@ create database task;
 
 use task; 
 
-create table user(
-	id int not null auto_increment,
-	user_id varchar(8),
-    username varchar(15),
-    name nvarchar(15),
-    constraint id_pk primary key(id)
-    constraint user_id_pk primary key(user_id)
-);
+-- create table user(
+-- 	id int not null auto_increment,
+-- 	user_id varchar(8),
+--     username varchar(15),
+--     name nvarchar(15),
+--     constraint id_pk primary key(id)
+--     constraint user_id_pk primary key(user_id)
+-- );
+
+
 
 create table user(
 	id int not null auto_increment,
@@ -23,6 +25,15 @@ create table user(
     constraint user_id_pk primary key(user_id),
     key (id)
 );
+
+-- create table user(
+-- 	id int not null auto_increment,
+-- 	user_id varchar(8),
+--     username varchar(15),
+--     name nvarchar(15),
+--     constraint username_pk primary key(username),
+--     key (id)
+-- );
 
 select * from user; 
 
@@ -38,6 +49,17 @@ create table task(
     constraint user_id_fk foreign key (user_id) references user(user_id)
 );
 
+-- create table task(
+-- 	task_id int not null auto_increment,
+--     description varchar(255), 
+--     priority int not null check (priority between 1 and 3),
+--     due_date date,
+--     user_id varchar(15),
+-- 	primary key(task_id),
+--     constraint username_fk foreign key (username) references user(username)
+-- );
+
+drop table task; 
 select * from task; 
 
 
